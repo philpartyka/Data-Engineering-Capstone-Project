1f994f27fd25cd17cd72168e4212fffda17e77cc
Philip Partyka <philpartyka@gmail.com>
2023-07-09 23:34:47 -0400

    gitignore test

--- credentials.py
+++ credentials.py
@@ -1,6 +1,6 @@
 
 #Credentials used to login to the MySQL server
-host_name = '# IGNORElocalhost# END IGNORE'
+host_name = IGNORE 'localhost' EIGNORE
 user_name = '# IGNOREroot# END IGNORE'
 password = '# IGNOREpassword# END IGNORE'
 port = '# IGNORE3306# END IGNORE'
