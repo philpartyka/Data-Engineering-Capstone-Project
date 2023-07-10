fd69f878dc814cba98cf68fdc572d2490e1536e1
Philip Partyka <philpartyka@gmail.com>
2023-07-09 23:30:21 -0400

    small changes

---
+++ credentials.py
@@ -0,0 +1,6 @@
+
+#Credentials used to login to the MySQL server
+host_name = '# IGNORElocalhost# END IGNORE'
+user_name = '# IGNOREroot# END IGNORE'
+password = '# IGNOREpassword# END IGNORE'
+port = '# IGNORE3306# END IGNORE'
